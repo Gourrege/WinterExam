5f16cace68ad33483b48d995f227ebe80dc12bb7
Henrick Gourrege <S00259704@atu.ie>
2025-01-08 13:21:12 +0000

    Added Ticket Class and VIP Ticket Child Class

---
+++ WinterExam/VIPTicket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterExam
+{
+    class VIPTicket:Ticket
+    {
+        //Properties
+
+        public string AdditionalExtras { get; set; }
+        public decimal AddiontalCost { get; set; }
+
+        //Constructors
+
+
+
+        //Methods
+    }
+}
